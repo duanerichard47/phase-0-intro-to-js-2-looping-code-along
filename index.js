c922c5cdedc15aea75903fecc6cc667ee389bba1
Duane <duanerichard47@hotmail.com>
2023-01-17 14:59:16 -0500

    incomplete pahasee -0- loop lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solutions in this file
+/*let newA = [];
+function writeCards(namesP,eventP)
+{
+for(let i=0;i<namesP.length;i++)
+    {
+        newA.push(`Thank you, ${namesP[i]}, for the wonderful ${eventP} gift!`);
+    }
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+console.log(newA)   */
+
+
+let newA = [];
+function writeCards(namesP,eventP)
+{
+   
+for(let i=0; i<namesP.length;  i++)
+    {
+       
+        newA.push(`Thank you, ${namesP[i]}, for the wonderful ${eventP} gift!`);
+    }
+    return newA
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+console.log(newA)
+
+
+
+
+
+
+let originalInt = 10;
+function countDown(intgerP)
+{ 
+    
+    while(intgerP >= 0)
+    {
+        console.log(intgerP);
+        intgerP--;
+    }
+
+}
+countDown(originalInt);
